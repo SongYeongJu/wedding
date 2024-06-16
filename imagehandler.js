d3db3a1b82f9f654b61d17c8b96bf019aac407e5
SongYeongJu <thddudwn1012@naver.com>
2024-06-16 22:18:22 +0900

    aaa

--- imagehandler.js
+++ imagehandler.js
@@ -1,12 +1,40 @@
 var modalImg;
 var currentIndex;
-var images;
+// var images;
+
+var original_images = [
+    "./img/original/1.jpg",
+    "./img/original/2.jpg",
+    "./img/original/3.jpg",
+    "./img/original/4.jpg",
+    "./img/original/5.jpg",
+    "./img/original/6.jpg",
+    "./img/original/7.jpg",
+    "./img/original/8.jpg",
+    "./img/original/9.jpg",
+    "./img/original/10.jpg",
+    "./img/original/11.jpg",
+    "./img/original/12.jpg",
+    "./img/original/13.jpg",
+    "./img/original/14.jpg",
+    "./img/original/15.jpg",
+    "./img/original/16.jpg",
+    "./img/original/17.jpg",
+    "./img/original/18.jpg",
+    "./img/original/19.jpg",
+    "./img/original/20.jpg",
+    "./img/original/21.jpg",
+    "./img/original/22.jpg",
+    "./img/original/23.jpg",
+    "./img/original/24.jpg",
+];
 
 function openModal(index) {
     document.getElementById('myModal').style.display = "block";
     modalImg = document.getElementById('modalImg');
-    images = document.querySelectorAll('.gallery img');
-    modalImg.src = images[index].src;
+    // images = document.querySelectorAll('.gallery img');
+    // modalImg.src = images[index].src;
+    modalImg.src = original_images[index];
     currentIndex = index;
 }
 
@@ -17,11 +45,11 @@ function closeModal() {
 function changeImage(direction) {
     currentIndex += direction;
     if (currentIndex < 0) {
-        currentIndex = images.length - 1;
-    } else if (currentIndex >= images.length) {
+        currentIndex = original_images.length - 1;
+    } else if (currentIndex >= original_images.length) {
         currentIndex = 0;
     }
-    modalImg.src = images[currentIndex].src;
+    modalImg.src = original_images[currentIndex];
 }
 
 document.addEventListener('keydown', function(event) {
