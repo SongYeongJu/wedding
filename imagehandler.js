6b9400b3993c05164d954e7c2fccf60bfdd45122
SongYeongJu <thddudwn1012@naver.com>
2024-06-16 22:54:55 +0900

    aaa

--- imagehandler.js
+++ imagehandler.js
@@ -1,6 +1,7 @@
+const gallery = document.getElementById('gallery');
+
 var modalImg;
 var currentIndex;
-// var images;
 
 var original_images = [
     "./img/original/1.jpg",
@@ -28,6 +29,35 @@ var original_images = [
     "./img/original/23.jpg",
     "./img/original/24.jpg",
 ];
+
+const my_images = [
+    "./img/prev/1.jpg",
+    "./img/prev/2.jpg",
+    "./img/prev/3.jpg",
+    "./img/prev/4.jpg",
+    "./img/prev/5.jpg",
+    "./img/prev/6.jpg",
+    "./img/prev/7.jpg",
+    "./img/prev/8.jpg",
+    "./img/prev/9.jpg",
+    "./img/prev/10.jpg",
+    "./img/prev/11.jpg",
+    "./img/prev/12.jpg",
+    "./img/prev/13.jpg",
+    "./img/prev/14.jpg",
+    "./img/prev/15.jpg",
+    "./img/prev/16.jpg",
+    "./img/prev/17.jpg",
+    "./img/prev/18.jpg",
+    "./img/prev/19.jpg",
+    "./img/prev/20.jpg",
+    "./img/prev/21.jpg",
+    "./img/prev/22.jpg",
+    "./img/prev/23.jpg",
+    "./img/prev/24.jpg",
+];
+
+loadImages();
 
 function openModal(index) {
     document.getElementById('myModal').style.display = "block";
@@ -63,4 +93,38 @@ document.addEventListener('keydown', function(event) {
         }
     }
 });
+        
+function createGalleryItem(src, index) {
+    const div = document.createElement('div');
+    div.className = 'image-container';
+    const img = document.createElement('img');
+    img.src = src;
+    img.alt = `이미지 ${index + 1}`;
+    img.loading="lazy";
+    img.onclick = () => openModal(index);
+    div.appendChild(img);
+    return div;
+}
 
+function loadImages() {
+    const button = document.getElementById('show_btn');
+    const displayStyle = window.getComputedStyle(button).display;
+
+    let show_all = false;
+    if (displayStyle === 'none') {
+        show_all = true;
+    }
+    
+    my_images.forEach((src, index) => {
+        if(show_all || index < 9) {
+            const galleryItem = createGalleryItem(src, index);
+            gallery.appendChild(galleryItem);
+        } 
+    });
+}      
+
+function show_more_img() {
+    const button = document.getElementById('show_btn');
+    button.style.display = "none";
+    loadImages();
+}
