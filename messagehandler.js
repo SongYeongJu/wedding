1ff2f9b58b0ae08fb89e7037bce76d506187f69a
SongYeongJu <thddudwn1012@naver.com>
2024-07-05 15:46:01 +0900

    test

--- messagehandler.js
+++ messagehandler.js
@@ -1,12 +1,12 @@
 console.log("test");
-fetch('config.json')
-    .then(response => response.json())
-    .then(config => {
-        const apiKey = config.API_KEY;
-        console.log("API Key: ", apiKey);
-        // Your code that uses the API key goes here
-    })
-    .catch(error => console.error('Error:', error));
+// fetch('config.json')
+//     .then(response => response.json())
+//     .then(config => {
+//         const apiKey = config.API_KEY;
+//         console.log("API Key: ", apiKey);
+//         // Your code that uses the API key goes here
+//     })
+//     .catch(error => console.error('Error:', error));
 
 
 // Firebase 설정
